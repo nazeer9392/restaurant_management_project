036e0a05f78ad73614a6f7442f51964b1ddfda7a
nazeer9392 <nazeershaik2784459@gmail.com>
2025-08-12 17:35:37 +0530

    30434

--- home/views.py
+++ home/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render
-
+from django.conf import settings
 # Create your views here.
  def about(request):
     return render(request,'about.html')
+def homepage_view(request):
+    context={
+        'phone_number':settings.RESTAU
+    }
