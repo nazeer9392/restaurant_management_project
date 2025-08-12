914b20ed3d696bb6742676b056f16cf0e2a6ff4c
nazeer9392 <nazeershaik2784459@gmail.com>
2025-08-12 17:51:04 +0530

    30429

--- restaurant_management/settings.py
+++ restaurant_management/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-s^x_u!itekxd=@a3o3zv4d%hw$j6d#2v358wgltxl8rc(-^t&p'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
@@ -128,3 +128,4 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 RESTAURENT_PHONE_NUMBER ="+1234567890"
+RESTAURENT_NAME='NazMyd'
